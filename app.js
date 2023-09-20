e02842aff9f654d5c67344bb45bdd0e5b9d239ba
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-09-20 12:15:11 +0400

    Изменения в CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use(cookieParser());
 
